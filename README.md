80663d602a17278af6d715a847de4e749790560a
Stepan Zinchenko <asworlddivide@gmail.com>
2016-10-06 22:47:41 +1000

    Global update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ai
+[![Node.JS](https://img.shields.io/badge/npm-%3E%3D4.0-brightgreen.svg)](https://nodejs.org)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
 AI Homework
+
+[![Gulp](https://raw.githubusercontent.com/gulpjs/artwork/master/gulp-2x.png)]
+[![Babel](https://raw.githubusercontent.com/gulpjs/artwork/master/gulp-2x.png)]
+[![ES6](https://blog.codewithdan.com/wp-content/uploads/2016/04/es6-logo-300x300-270x250.png)]
--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,25 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 var watch = require('gulp-watch');
 var ch = require('chalk');
-
+var del = require('del');
 
 var path = {
-  srcPath: 'js/src/**/*.js',
+  srcPath: 'js/src/*.js',
+  pathToWatch: 'js/src/**/*.js',
   destPath: 'js/dist'
 }
 
-gulp.task('default', ['ES6']);
+gulp.task('default', ['ES6', 'watch']);
+
+gulp.task('watch', () => {
+  gulp.watch(path.pathToWatch, ['ES6'], (e) => {
+    console.log(`[${ch.blue(new Date().toTimeString().split(' ')[0])}] ${ch.yellow(e.path.match(/(\w+\.js)/g))} was edited! Rebuild JS file...`)
+  })
+})
 
 gulp.task('ES6', () => {
   return gulp.src(path.srcPath)
-    .pipe(watch(path.srcPath, (e)=>{
-      console.log(`${ch.yellow(e.path.match(/(\w+\.js)/g))} was edited! Running compile task`)
-    }))
+    .pipe(require('gulp-include')())
     .pipe(babel())
-    .pipe(gulp.dest(path.destPath));
+    .pipe(gulp.dest(path.destPath))
 });
