6a5a3f2de11dffc05dd6e142eb6933daab1cfb89
Stepan Zinchenko <asworlddivide@gmail.com>
2016-10-20 17:14:56 +1000

    Update Env

--- js/src/modules/es5Features.js
+++ js/src/modules/es5Features.js
@@ -1,10 +1,18 @@
 Array.prototype.diff = function(a) {
   var diffArray = []
-  this.forEach(function (e, i) {
+  this.forEach(function(e, i) {
     if (e !== a[i]) diffArray.push(e)
   })
   return diffArray
 }
-Array.prototype.swap = function (a, b) {
+Array.prototype.swap = function(a, b) {
   return [this[a], this[b]] = [this[b], this[a]]
+}
+
+Array.prototype.max = function() {
+  return Math.max.apply(null, this);
+}
+
+Array.prototype.min = function() {
+  return Math.min.apply(null, this);
 }
